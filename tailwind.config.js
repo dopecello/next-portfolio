c5d4ab8c436e5c928430fdfd10681fbc3a8adc3e
dopecello <williamlocke.cello@gmail.com>
2023-03-29 16:25:41 -0400

    added 'xs' screen size to custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '375px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
 };
+
+
+//iphonese : 375px
