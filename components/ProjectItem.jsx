e184d9be7027f0e6b46eb46f89e81f43ad3dd6f9
dopecello <williamlocke.cello@gmail.com>
2023-04-01 19:32:38 -0400

    finalizing ProjectItem component

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const ProjectItem = ({ title, alt, tech, bgImg, projectURL }) => {
     return (
-        <div className='bg-gray-100 hover:bg-gradient-to-r from-[#04348B] to-[#0854e2d0] relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group'>
+        <div className='bg-white border hover:bg-gradient-to-r from-[#04348B] to-[#0854e2d0] relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-200 rounded-xl p-4 group'>
             <Image src={bgImg} alt={alt} className='rounded-xl group-hover:opacity-10' />
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                 <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
