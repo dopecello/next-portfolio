5bfb3e151bef7c0e733e189df3f55e6ca78886b6
dopecello <williamlocke.cello@gmail.com>
2023-04-04 17:54:58 -0400

    added icons to project array

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaExternalLinkAlt, FaGithubSquare } from 'react-icons/fa'
 
-const ProjectItem = ({ title, alt, tech, bgImg, projectURL }) => {
+const ProjectItem = ({ title, alt, tech, bgImg, projectURL, githubURL }) => {
     return (
         <div className='bg-white border hover:bg-gradient-to-r from-[#04348B] to-[#0854e2d0] relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-200 rounded-xl p-4 group'>
             <Image src={bgImg} alt={alt} className='rounded-xl group-hover:opacity-10' />
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                 <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
                 <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
-                <Link href={projectURL}>
-                    <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg'>More Info</p>
-                </Link>
+                <div className='flex justify-center gap-6'>
+                    <div className='rounded-full shadow-lg bg-slate-100 p-4 cursor-pointer hover:scale-105 ease-in duration-300'>
+                        <Link href={githubURL} title='GitHub Repo' target='_blank' rel='noopener noreferrer'>
+                            <FaGithubSquare size={30} />
+                        </Link>
+                    </div>
+                    <div className='rounded-full shadow-lg bg-slate-100 p-4 cursor-pointer hover:scale-105 ease-in duration-300'>
+                        <Link href={projectURL} title='Deployed Project' target='_blank' rel='noopener noreferrer'>
+                            <FaExternalLinkAlt size={30} />
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -19,3 +29,5 @@ const ProjectItem = ({ title, alt, tech, bgImg, projectURL }) => {
 }
 
 export default ProjectItem
+
+//I want to add the FaGithubSquare icon and FaExternalLinkAlt
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,14 +6,14 @@ import ProjectItem from './ProjectItem'
 const Projects = () => {
 
     const projItemArr = [
-        {title: "OpenWeather API Forecast Checker", alt: "Weather Forecasting Application", tech: "Next.js", bgImg: weatherApp, projectURL: "https://dopecello.github.io/weather-app/"},
-        {title: "SupaHot Online Marketplace", alt: "SupaHot Online Marketplace", tech: "CRA, GraphQL, MongoDB", bgImg: firesale, projectURL: "https://supa-hot-firesales.herokuapp.com/"}, 
+        { title: "OpenWeather API Forecast Checker", alt: "Weather Forecasting Application", tech: "JavaScript, Bulma", bgImg: weatherApp, projectURL: "https://dopecello.github.io/weather-app/", githubURL: "https://github.com/dopecello/weather-app" },
+        { title: "SupaHot Online Marketplace", alt: "SupaHot Online Marketplace", tech: "CRA, GraphQL, MongoDB", bgImg: firesale, projectURL: "https://supa-hot-firesales.herokuapp.com/", githubURL: "https://github.com/brian-gee/supa-hot-firesale" },
     ]
 
     return (
         <div id='projects' className='w-full xl:pt-[13rem] xl:pb-[5rem]'>
             <div className='max-w-[1240px] mx-auto px-2 pt-10 pb-10'>
-                <p className='text-xl tracking-widest uppercase text-lite-blue'>Projects</p>
+                <p className='text-xl tracking-widest uppercase py-6 text-lite-blue'>Projects</p>
                 <div className='grid md:grid-cols-2 gap-8'>
                     {projItemArr.map((index) => {
                         return (
@@ -22,6 +22,7 @@ const Projects = () => {
                                 alt={index.alt}
                                 tech={index.tech}
                                 bgImg={index.bgImg}
+                                githubURL={index.githubURL}
                                 projectURL={index.projectURL}
                             />
                         )
