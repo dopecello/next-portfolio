86dac3941fd5d7357ae8f7467f0045a2e60e9d33
dopecello <williamlocke.cello@gmail.com>
2023-04-22 14:36:57 -0400

    added projects to projects section before deployment

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import weatherApp from '../public/assets/weatherapp.png'
 import firesale from '../public/assets/firesale-final.png'
 import ProjectItem from './ProjectItem'
+import mealfind from '../public/assets/mealfind-sized.png'
+import opus from '../public/assets/opus.png'
 
 const Projects = () => {
 
     const projItemArr = [
         { title: "OpenWeather API Forecast Checker", alt: "Weather Forecasting Application", tech: "JavaScript, Bulma", bgImg: weatherApp, projectURL: "https://dopecello.github.io/weather-app/", githubURL: "https://github.com/dopecello/weather-app", id: 1 },
         { title: "SupaHot Online Marketplace", alt: "SupaHot Online Marketplace", tech: "CRA, GraphQL, MongoDB", bgImg: firesale, projectURL: "https://supa-hot-firesales.herokuapp.com/", githubURL: "https://github.com/brian-gee/supa-hot-firesale", id: 2 },
+        { title: "MealFind", alt: "MealFind", tech: "TypeScript, Next.js, RESTful API, SQL", bgImg: mealfind, projectURL: "https://github.com/dopecello/meal-find", githubURL: "https://github.com/dopecello/meal-find", id: 3 },
+        { title: "Opus Music Worldwide", alt: "Opus Music Worldwide", tech: "TypeScript, OAuth, Firebase, TailwindCSS", bgImg: opus, projectURL: "opus-worldwide.vercel.app", githubURL: "https://github.com/dopecello/music-jobs", id: 4 }
     ]
 
     return (
